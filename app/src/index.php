5b91a0479539dc8d07d67d1b1d85133e37942e56
Kevin Jones <kevin@shopblocks.com>
2024-11-23 23:23:13 +0000

    Updated deployment to version 0.0.13

--- app/src/index.php
+++ app/src/index.php
@@ -3,6 +3,9 @@
 require 'bootstrap.php';
 
 $version = getenv('BUILD_VERSION');
+if (!empty($_GET['sleep']) && is_numeric($_GET['sleep'])) {
+    sleep($_GET['sleep']);
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,5 +18,10 @@ $version = getenv('BUILD_VERSION');
         <p>Hello from the GitOps Example PHP Application v<?= $version ?></p>
         <p style="color:blue">It is <?= date("Y-m-d H:i:s") ?></p>
         <p><a href="/info.php">PHP INFO</a></p>
+        <div>
+            <pre>
+                <?php print_r($_SERVER); ?>
+            </pre>
+        </div>
     </body>
 </html>
