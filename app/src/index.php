45fd3f21f6da555dfd6f1c465d44f52b2849c0a2
Kevin Jones <kevin@shopblocks.com>
2024-11-23 20:09:18 +0000

    code change

--- app/src/index.php
+++ app/src/index.php
@@ -13,7 +13,7 @@ $version = getenv('BUILD_VERSION');
     <body>
         <h1>GitOps Example</h1>
         <p>Hello from the GitOps Example PHP Application v<?= $version ?></p>
-        <p>It is <?= date("Y-m-d H:i:s") ?></p>
+        <p style="color:blue">It is <?= date("Y-m-d H:i:s") ?></p>
         <p><a href="/info.php">PHP INFO</a></p>
     </body>
 </html>
