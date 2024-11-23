0f1ce8d93f008792d0a8840eda7657a515918f1d
Kevin Jones <kevin@shopblocks.com>
2024-11-23 17:14:55 +0000

    Changed PHP

--- app/src/index.php
+++ app/src/index.php
@@ -1,2 +1,3 @@
 <?php
-echo "Hello from the GitOps Example PHP Application. It is " . date("Y-m-d H:i:s") . ".";
+$version = '0.0.1';
+echo "Hello from the GitOps Example PHP Application v$version. It is " . date("Y-m-d H:i:s") . ".";
