6592291aee2bfdcc6ff5930b8351a0e85055257a
Kevin Jones <kevin@shopblocks.com>
2024-11-23 18:05:43 +0000

    app update

--- app/src/index.php
+++ app/src/index.php
@@ -1,7 +1,16 @@
 <?php
-$version = '0.0.1';
+$version = '0.0.2';
 ?>
-<h1>GitOps Example</h1>
-<p>Hello from the GitOps Example PHP Application v<?= $version ?></p>
-<p>It is <?= date("Y-m-d H:i:s") ?></p>
-<p><a href="/info.php">PHP INFO</a></p>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>GitOps Example</title>
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    </head>
+    <body>
+        <h1>GitOps Example</h1>
+        <p>Hello from the GitOps Example PHP Application v<?= $version ?></p>
+        <p>It is <?= date("Y-m-d H:i:s") ?></p>
+        <p><a href="/info.php">PHP INFO</a></p>
+    </body>
+</html>
